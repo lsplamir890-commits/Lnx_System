fba26ba6aa6a63e8927b5405d88dc0b441c811d3
Jsasn <lsplamir890@gmail.com>
2026-09-21 23:37:09 +0200

    added core files
    This aint important

---
+++ Kernel.cs
@@ -0,0 +1,407 @@
+using System;
+using Sys = Cosmos.Kernel.System;
+using System.IO;
+using Cosmos.Kernel.System.Storage;
+using Cosmos.Kernel.System.Vfs;
+using Cosmos.Kernel.System.Filesystems.Fat;
+using Cosmos.Kernel.HAL.Interfaces.Devices;
+using Cosmos.Kernel.HAL.Vfs;
+using Cosmos.Kernel.System;
+using assembly = System.Reflection.Assembly;
+using Cosmos.Kernel.System.Diagnostics;
+using Cosmos.Kernel.System.Filesystems.Ext2;
+namespace lnkrnl;
+
+/// <summary>
+/// Main kernel class - inherits from Cosmos.Kernel.System.Kernel.
+/// </summary>
+public class Kernel : Sys.Kernel
+{
+    
+    static string DefaultPartName = ReadIni("/mnt/LunDos.ini", "FsName");
+    Stream? resource = assembly.GetExecutingAssembly().GetManifestResourceStream("promaxkernel.EmbeddedFile.txt");
+    static string ReadIni(string Path, string target)
+    {
+        string[] mainsetupfile = File.ReadAllLines("/mnt/LunDos.ini"); 
+        foreach(string line in mainsetupfile)
+        {
+            if(line.Contains(target))
+            {
+                if(line.Contains("="))
+                {
+                    string output = line.Split("=")[1];
+                    return output;
+                }
+                
+            }
+           
+            
+        }
+        return "";
+    }
+    static void createfile(string name, string conent)
+    {
+        string currdir = Directory.GetCurrentDirectory();
+        string filePath = Path.Combine(currdir, name);
+        try
+        {
+            using FileStream stream = File.Create(filePath);
+             File.WriteAllText(filePath, conent);
+        } 
+        catch
+        {
+            Console.WriteLine("Failed to create file: " + name);
+        }
+        
+    }
+    protected override void BeforeRun()
+    {
+        IBlockDevice? disk = StorageManager.PrimaryDevice;
+        FatFilesystemType fat = new();
+
+        if (!VfsManager.RegisterFilesystem("fat", fat))
+        {
+            Console.WriteLine("The name \"fat\" is already registered.");
+            return;
+        }
+        Gpt.Create(disk);
+        if (!PartitionManager.Create(disk, startSector: 2048, sectorCount: 131072,
+                                    mbrSystemId: 0x0C, gptType: Gpt.BasicDataPartitionType))
+        {
+            Console.WriteLine("Create failed");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Partition created successfully.");
+        }
+        StorageManager.RescanPartitions(disk);
+        if (StorageManager.Partitions.Count == 0)
+        {
+            Console.WriteLine("No partitions found.");
+            return;
+        }
+        if (VfsManager.TryMount("fat", StorageManager.Partitions[0], MountFlags.None, "/mnt", out VfsManager.VfsMount? mount))
+        {
+            Console.WriteLine("Mounted " + mount.Name + " at " + mount.MountPoint);
+        }
+        if(!File.Exists("/mnt/instLuviz.lze"))
+        {
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            // using var reader = new StreamReader(resource);
+            // string content = reader.ReadToEnd();
+            // Console.WriteLine("Embedded file content: " + content);
+
+            Console.WriteLine("SETUP LOADED IN: ");
+            Console.WriteLine("You must have a nvme drive or achi drive for the setup to work");
+            Console.WriteLine("Press F2 to start setup");
+            Console.WriteLine("Press F3 to reformat or format the drive(format the disk if you never did)"); 
+            ConsoleKeyInfo  key = Console.ReadKey(true);
+            switch(key.Key)
+            {
+                case ConsoleKey.F2:
+                    try
+                    {
+                        Console.WriteLine("Starting setup...");
+                        File.Create("/mnt/instLuviz.lze");
+                        File.AppendAllText("/mnt/instLuviz.lze", "[systeminstall] = true");
+                        Directory.CreateDirectory("/mnt/SysSetts/files");
+                        File.AppendAllText("/mnt/LunDos.ini","[setup]\n");
+
+                        //name
+                        Console.Write("Type your name: ");
+                        string user = Console.ReadLine().Trim();
+                        File.AppendAllText("/mnt/LunDos.ini",$"username={user}\n");
+            
+                        //filesystem
+                        Console.Write("Default partition name:");
+                        string Fs = Console.ReadLine().Trim();
+                        File.AppendAllText("/mnt/LunDos.ini",$"FsName={Fs}\n");
+
+                        Console.WriteLine("Setup completed. It is safe to reboot your pc. DO NOT REMOVE THE CD OR USB");
+                        Power.Shutdown();
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("SYSTEM HALTED. Couldnt Setup your pc. Please restart and DO NOT REMOVE THE CD OR USB\nextra info: This may have happend because you didnt format your disk or\nit isnt supported") ;
+                        Power.Shutdown();
+                    }
+                    break;
+                case ConsoleKey.F3:
+                    try
+                    {
+                        Console.WriteLine("Starting formatting/reformmating...");
+                        Console.WriteLine("Formatting /mnt/ partition...");
+                        VfsManager.TryUnmount("/mnt");
+                        FatFormatOptions options = new()
+                        {
+                            Type = FatType.Fat32,
+                            VolumeLabel = $"COSMOS     ",
+                        };
+
+                        if (StorageManager.Partitions.Count == 0
+                            || !VfsManager.TryFormat("fat", StorageManager.Partitions[0], options))
+                        {
+                            Console.WriteLine("Format failed");
+                        }   
+                        VfsManager.TryMount("fat", StorageManager.Partitions[0], MountFlags.None, "/mnt", out VfsManager.VfsMount? mountO);
+                    
+                        Console.WriteLine("Formatting completed. It is safe to reboot your pc. DO NOT REMOVE THE CD OR USB");
+                        Power.Shutdown();
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("SYSTEM HALTED. could not format your disk. Please restart and DO NOT REMOVE THE CD OR USB") ;
+                        Power.Shutdown();
+                    }
+
+                    
+                    break;
+                    default:
+                    Console.WriteLine("Couldnt install since you Didnt press f2 or f3 It is safe to reboot your pc. DO NOT REMOVE THE CD OR USB");
+                    break;
+                
+            }
+        }
+        try
+        {
+             Directory.SetCurrentDirectory("/mnt");
+        }
+        catch
+        {
+            Console.WriteLine("Couldnt set the current directory to Primary partition");
+        }
+        Console.WriteLine("Please wait..");
+        Thread.Sleep(1500);
+        Console.Clear();
+        if (VfsManager.TryStatFs("/mnt", out VfsStatFs stats))
+        {
+            ulong freeBytes = stats.Bavail * stats.BlockSize;
+            ulong totalBytes = stats.Blocks * stats.BlockSize;
+            Console.WriteLine($"{freeBytes} bytes of hdd left [OK]");
+        }
+        Console.WriteLine("KERNEL booted successfully!");
+        Console.WriteLine("tip: If you want to see all commands type 'help' and press enter.\nAlso This is a beta under Development ");
+
+    }
+    static string username = ReadIni("/mnt/LunDos.ini", "username");
+    protected override void Run()
+    {
+
+        
+        try
+        {
+           
+
+            Console.Write($"{username}@~ $ {Directory.GetCurrentDirectory()} > ");
+        }
+        catch
+        {
+            Console.Write("root@~ $ UNKNOWNDRIVE > ");
+        }
+        
+        var input = Console.ReadLine().ToLower();
+
+        if (string.IsNullOrEmpty(input))
+            return;
+        if(input.StartsWith("mkdir -p "))
+        {
+            string directory = input.Substring("mkdir -p ".Length).Trim();
+            try
+            {
+                Directory.CreateDirectory(directory);
+                Console.WriteLine($"Directory '{directory}' created successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to create directory '{directory}'");
+            }
+            return;
+        }
+        else if(input.StartsWith("cd "))
+        {
+            string directory = input.Split(" ")[1].Trim();
+            try
+            {
+                
+                Directory.SetCurrentDirectory($"{Directory.GetCurrentDirectory()}/{directory}");
+                
+            }
+            catch
+            {
+                Console.WriteLine($"{directory} is missing or its corrupt and damaged");
+            }
+            return;
+        } 
+        else if(input.StartsWith("del "))
+        {
+            string sector = input.Split(" ")[1].Trim();
+            try
+            {
+                if(File.Exists(sector))
+                {
+                     File.Delete($"{sector}");
+                }
+                else
+                {
+                    Directory.Delete($"{sector}", true);
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"This isnt a valid file/folder or it  doesnt exist");
+            }
+            return;
+        }     
+        else if(input.StartsWith("echo > "))
+        {
+            string sector = input.Split(" ")[2].Trim();
+            try
+            { 
+                Console.WriteLine(File.ReadAllText($"{Directory.GetCurrentDirectory()}/{sector}"));
+
+            }
+            catch
+            {
+                Console.WriteLine($"This isnt a valid file or it  doesnt exist");
+            }
+            return;
+        }   
+        else if(input.StartsWith("echo < "))
+        {
+            string filename = input.Split(" ")[2].Trim();
+            try
+            {
+                
+                Console.WriteLine("Type the text :");
+                string  text = Console.ReadLine();
+                File.WriteAllText($"{Directory.GetCurrentDirectory()}/{filename}", text);
+            }
+            catch
+            {
+                Console.WriteLine($"Couldnt make a new file");
+            }
+            return;
+        }
+        else if(input.StartsWith("echo "))
+        {
+            string filename = input.Substring(5);
+            Console.WriteLine(filename);
+            return;
+        }
+        switch (input.ToLower())
+        {
+            case "date":
+            
+                Console.WriteLine($"{DateTime.Now}");
+            break;
+            case "format":
+                VfsManager.TryUnmount("/mnt");
+                FatFormatOptions options = new()
+                {
+                    Type = FatType.Fat32,
+                    VolumeLabel = $"{DefaultPartName}     ",
+                };
+
+                if (StorageManager.Partitions.Count == 0
+                    || !VfsManager.TryFormat("fat", StorageManager.Partitions[0], options))
+                {
+                    Console.WriteLine("Format failed");
+                }   
+                VfsManager.TryMount("fat", StorageManager.Partitions[0], MountFlags.None, "/mnt", out VfsManager.VfsMount? mount);
+                
+            break;
+            case "format -v ext2":
+                VfsManager.TryUnmount("/mnt");
+                Ext2FormatOptions option = new()
+                {
+                    VolumeLabel = $"{DefaultPartName}     ",
+                };
+
+                if (StorageManager.Partitions.Count == 0
+                    || !VfsManager.TryFormat("ext2", StorageManager.Partitions[0], option))
+                {
+                    Console.WriteLine("Format failed");
+                }   
+                VfsManager.TryMount("ext2", StorageManager.Partitions[0], MountFlags.None, "/mnt", out VfsManager.VfsMount? mount1);
+                
+            break;
+            case "maketestfile":
+            try
+            {
+                using FileStream stream = File.Create("/mnt/testing.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            break;
+            case "ls":
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            string[] dirs = Directory.GetDirectories(Directory.GetCurrentDirectory());
+
+            Console.Write("|Files| \n");
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.Write("|Directories| \n");
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine( dir);
+            }
+
+            break;
+            
+            case "help":
+                Console.WriteLine("Available commands:");
+                Console.WriteLine("  echo > <filename> - Read a file and it displays text");
+                Console.WriteLine("  echo < <filename> - Writes a file to the primary partition");
+                Console.WriteLine("  format   - Format the primary partition with FAT32");
+                Console.WriteLine("  maketestfile - Create a test file in the root directory");
+                Console.WriteLine("  ls      - List files in the current directory");
+                Console.WriteLine("  mkdir -p <directory> - Create a directory ");
+                Console.WriteLine("  help     - Show this help message");
+                Console.WriteLine("  del <file/directory>    - Deletes a file or directory");
+                Console.WriteLine("  cd <directory>     - changes directory");
+                Console.WriteLine("  clear    - Clear the screen");
+                Console.WriteLine("  halt     - Halt the system");
+                Console.WriteLine("  date     - shows the date"); 
+                Console.WriteLine("  ver     - Shows the os version");
+                Console.WriteLine("  abt     - Shows some data");
+                Console.WriteLine("  echo     - echos the text you inputed");
+                break;
+            case "ver":
+                Console.WriteLine("RELEASE!!! Thanks for trying this os");
+            break;
+            
+            case "abt":
+             Console.WriteLine("Made by jsplashh");
+             Console.WriteLine("Simple but a little creative os");
+             Console.WriteLine("Kernel: lnkrnl\nKernel Achitecture: ln");
+             Console.WriteLine("Thanks for reading this and trying this");
+             Console.WriteLine("Made on: Gen 3 v3.0.87 COSMOS");
+            break;
+            case "clear":
+                Console.Clear();
+                break;
+
+            case "halt":
+                Console.WriteLine("Halting system...");
+                Stop();
+                break;
+            
+            default:
+                Console.WriteLine($"\"{input}\" is not a command");
+                break;
+            
+        }
+        GC.Collect();
+    }
+}
